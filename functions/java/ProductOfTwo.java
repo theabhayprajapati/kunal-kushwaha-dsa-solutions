16523d59af5f9b0988c6fc3be8d3c2dae9e8ab6c
theabhayprajapati <theabhayprajapati@gmail.com>
2022-03-29 14:25:06 +0530

    Done with product

---
+++ functions/java/ProductOfTwo.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class ProductOfTwo {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        double num1 = in.nextDouble();
+        double num2 = in.nextDouble();
+        double Product = getProduct(num1, num2);
+        System.out.println(Product);
+        
+    }
+
+    static double getProduct(double num1, double num2) {
+        return num1 * num2;
+    }
+}
