7ceae200bd65575b103ae1f84bc626837a098921
theabhayprajapati <theabhayprajapati@gmail.com>
2022-03-29 16:25:17 +0530

    solved bug from its

--- functions/hava/Palidrome.java
+++ functions/hava/Palidrome.java
@@ -1,4 +1,5 @@
 package hava;
+
 // Q 1: Write a function to find if a number is a palindrome or not. Take number as parameter.
 import java.util.Scanner;
 
@@ -9,20 +10,24 @@ public class Palidrome {
         in.close();
         boolean check = checkPalidrome(num);
         System.out.println(check);
-
     }
 
     static boolean checkPalidrome(int num) {
         int pal = 0;
-        while (pal < num) {
-            num = num%10;
-            pal = pal * 10 + num;
-            System.out.println(num + "NUM");
+        int real = num;
+        while (num > 0) {
+            int r = num % 10;
+            pal = (pal * 10) + r;
+            num = num / 10;
         }
-        System.out.println(pal + "PAl");
-        if (pal == num) {
+        // System.out.println(pal + "PAl");
+        if (pal == real) {
+            System.out.println("iT'S pALIDINOE NUMSNUE");
+            
             return true;
         } else {
+            System.out.println("Use your braing and write properly it's not by the wya");
+            
             return false;
         }
 
