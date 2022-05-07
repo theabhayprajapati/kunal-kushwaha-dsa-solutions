3ecb1e567549d55d5559ab39386420d519ef3ccd
theabhayprajapati <theabhayprajapati@gmail.com>
2022-05-07 10:34:43 +0530

    updated array

--- arrays/Contationationofarray.java
+++ arrays/Contationationofarray.java
@@ -7,16 +7,14 @@ import javax.swing.text.html.HTMLDocument.RunElement;
 public class Contationationofarray {
     public static void main(String[] args) {
         int[] nums = {1,2,1};
-        
+        getConcatenation(nums);
     }
 
     static int[] getConcatenation(int[] nums) {
         int[] ans = new int[nums.length * 2];
         for (int i = 0; i < nums.length; i++) {
             ans[i] = nums[i];
-            for (int j = 0; j < nums.length; j++) {
-                ans[i + nums.length] = nums[i];
-            }
+            ans[i + nums.length] = nums[i];
         }
         return ans;
     }
