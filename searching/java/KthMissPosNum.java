2ea1538d3f38c85d8d68381fe8c7acc5922ce9c8
theabhayprajapati <theabhayprajapati@gmail.com>
2022-06-25 14:47:27 +0530

    solved kth missing number by someones answer in discuss section

--- searching/java/KthMissPosNum.java
+++ searching/java/KthMissPosNum.java
@@ -1,20 +1,38 @@
 package searching.java;
 
-//https://leetcode.com/problems/kth-missing-positive-number/
 /**
  * KthMissPosNum
  */
 public class KthMissPosNum {
 
     public static void main(String[] args) {
-        int[] nums = { 2, 3, 4, 7, 11 };
-        System.out.println(kthMissPosNum(nums, 5));
+        int[] arr = { 2, 3, 4, 7, 11 };
+        int k = 5;
+        int res = findKthMissPosNum(arr, k);
+        System.out.println(res);
     }
 
-    public static int kthMissPosNum(int[] nums, int i) {
-        int[] count = new int[i + 1];
-        // Sol:
-        int currentValue = 1;
-        int index = 0;
+    public static int findKthMissPosNum(int[] arr, int k) {
+        int n = arr.length;
+        int left = 0;
+        int right = n - 1;
+        int missing = compute(arr[n - 1], n);
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            missing = compute(arr[mid], mid + 1);
+            if (missing >= k) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        if (right == -1) {
+            return k;
+        }
+        return arr[right] + k - compute(arr[right], right + 1);
+    }
+
+    static int compute(int actual, int expected) {
+        return (actual - expected);
     }
 }
